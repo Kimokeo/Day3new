b028ddcbedf238e1c513c2f06c97c667ecb9788b
Kimokeo Rabang <kimokerava@gmail.com>
2015-01-07 14:21:07 -0500

    working on loop

--- Guess.rb
+++ Guess.rb
@@ -4,13 +4,21 @@
 # user has 5 chances
 
 
-(answer.to_i) = rand(100)
+answer = rand(100)
+guesses = 0
+
+
+loop do
 puts "Take a guess, numbers 1 - 100"
-guess = gets.chomp
-	if guess = answer
-		puts "victory!"
-	elsif guess > answer
-		puts "Too high"
-	elsif guess < answer
-		puts "Too low"
+guess = gets.chomp.to_i
+guesses += 1
+
+unless guess = answer
+	if guess > answer
+		puts "Too high, try again"
+	else
+		puts "Too low, try again"
+	end
+else
+puts "victory!"
 end
