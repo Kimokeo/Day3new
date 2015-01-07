b5f2d47ffdeeff7b84e92d2d37ebd6ff34b4bd99
Kimokeo Rabang <kimokerava@gmail.com>
2015-01-07 15:19:15 -0500

    need a little polishing

--- Guess.rb
+++ Guess.rb
@@ -1,28 +1,24 @@
-# computer picks a number
-# user gueses
-# computer tells if high or low
-# user has 5 chances
-
-
 answer = rand(100)
 guesses = 0
 
-
 loop do
-when guesses < 5
+if guesses < 5
 	puts "Take a guess, numbers 1 - 100"
 	guess = gets.chomp.to_i
 	guesses += 1
 
 	unless guess == answer
-		puts "victory! it took you #{guesses} tries"
-	else
 		if guess > answer
 			puts "Too high, try again"
 		else
 			puts "Too low, try again"
 		end
+	else
+	puts "victory! it took you #{guesses} tries"
+	break
 	end
+else
 puts "you lose try again"
+break
 end
-
+end
